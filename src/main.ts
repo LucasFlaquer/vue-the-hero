7c3aff7b852f4a0498c09055686594cdf55ec29a
Lucas Flaquer <lucas.flaquer@timeprimo.com>
2022-01-30 23:23:14 -0300

    routes and feather icons

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import { routes } from './routes';
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./index.css";
 
-createApp(App).mount("#app");
+import "./index.css";
+createApp(App)
+  .use(routes)
+  .mount("#app");
