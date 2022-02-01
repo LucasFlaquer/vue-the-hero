4cadd88d86776ac35a0dbb7495ac24563048c354
Lucas Flaquer <lucas.flaquer@timeprimo.com>
2022-01-31 23:45:27 -0300

    lint fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        'background': '#F0F0F5',
-        'redHero': '#E02041'
+        background: '#F0F0F5',
+        redHero: '#E02041',
       },
       fontFamily: {
-        'default': ['Roboto', 'sans-serif', 'Helvetica']
-      }
+        default: ['Roboto', 'sans-serif', 'Helvetica'],
+      },
     },
   },
   plugins: [],
