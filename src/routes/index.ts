bac70d6ffe66d30421386d9f9869b42b1941c864
Lucas Flaquer <lucas.flaquer@timeprimo.com>
2022-02-07 20:39:59 -0300

    routes and created listCases view

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import Cases from '../views/ListCases.vue';
 
 const routes = createRouter({
   history: createWebHistory(),
@@ -8,6 +9,11 @@ const routes = createRouter({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/cases',
+      name: 'cases',
+      component: Cases,
     },
   ],
 });
---
+++ src/views/ListCases.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container h-screen max-w-6xl mx-auto">
+    <header class="flex items-start justify-between pt-6 mb-20">
+      <div class="flex items-center">
+        <img src="../assets/hero-logo-small.svg" alt="Logo" />
+        <p class="ml-12 font-default font-normal text-xl text-mainTitle">
+          Bem vinda, APAD
+        </p>
+      </div>
+      <div class="flex">
+        <DefaultButton> Cadastrar novo caso </DefaultButton>
+        <button class="ml-6 border-2 text-center p-3 h-14 w-14 rounded-sm">
+          <power-icon
+            size="1.5x"
+            class="inline-block text-redHero align-bottom"
+          />
+        </button>
+      </div>
+    </header>
+    <main>
+      <MainTitle class="mb-6">Casos Cadastrados</MainTitle>
+      <section>saa casos</section>
+    </main>
+  </div>
+</template>
+<script setup>
+import { PowerIcon } from '@zhuowenli/vue-feather-icons';
+import MainTitle from '../components/MainTitle.vue';
+import DefaultButton from '../components/DefaultButton.vue';
+</script>
