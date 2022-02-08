422f9237bb4ac67e12c411afd206aea7f50bafd9
Lucas Flaquer <lucas.flaquer@timeprimo.com>
2022-02-07 22:36:53 -0300

    card cases

--- src/views/ListCases.vue
+++ src/views/ListCases.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container h-screen max-w-6xl mx-auto">
+  <div class="container h-full max-w-6xl mx-auto">
     <header class="flex items-start justify-between pt-6 mb-20">
       <div class="flex items-center">
         <img src="../assets/hero-logo-small.svg" alt="Logo" />
@@ -9,7 +9,9 @@
       </div>
       <div class="flex">
         <DefaultButton> Cadastrar novo caso </DefaultButton>
-        <button class="ml-6 border-2 text-center p-3 h-14 w-14 rounded-sm">
+        <button
+          class="ml-6 border text-center p-3 h-14 w-14 rounded-lg border-grayborder"
+        >
           <power-icon
             size="1.5x"
             class="inline-block text-redHero align-bottom"
@@ -19,7 +21,9 @@
     </header>
     <main>
       <MainTitle class="mb-6">Casos Cadastrados</MainTitle>
-      <section>saa casos</section>
+      <section class="grid grid-cols-2 gap-8 pb-20">
+        <CardCase v-for="index in 10" :key="index" />
+      </section>
     </main>
   </div>
 </template>
@@ -27,4 +31,5 @@
 import { PowerIcon } from '@zhuowenli/vue-feather-icons';
 import MainTitle from '../components/MainTitle.vue';
 import DefaultButton from '../components/DefaultButton.vue';
+import CardCase from '../components/CardCase.vue';
 </script>
