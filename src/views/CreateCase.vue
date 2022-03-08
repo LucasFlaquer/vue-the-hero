30f2b65495c5cd0b2b2c40f73cf56b6a7bc5ee3f
Lucas Flaquer <lucas.flaquer@timeprimo.com>
2022-03-08 19:07:01 -0300

    createCase

--- src/views/CreateCase.vue
+++ src/views/CreateCase.vue
@@ -28,22 +28,25 @@
           <strong>Voltar para home</strong>
         </router-link>
       </div>
-      <form class="basis-3/6">
+      <form class="basis-3/6" @submit.prevent="createCase">
         <input
           class="h-14 px-2 py-2 rounded-lg w-full mb-2"
           type="text"
           placeholder="Título do caso"
+          v-model="title"
         />
         <textarea
           class="px-2 py-2 rounded-lg w-full mb-2"
           type="text"
           rows="5"
           placeholder="Descrição"
+          v-model="description"
         />
         <input
           class="h-14 px-2 py-2 rounded-lg w-full mb-2"
           type="text"
           placeholder="Valor em reais"
+          v-model="ammount"
         />
         <div class="flex items-center justify-between">
           <button
@@ -61,6 +64,7 @@
               'duration-200',
               'basis-1/3',
             ]"
+            @click.prevent="goBack"
           >
             cancelar
           </button>
@@ -76,5 +80,19 @@
 </template>
 <script setup lang="ts">
 import { ArrowLeftIcon } from '@zhuowenli/vue-feather-icons';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 import MainTitle from '../components/MainTitle.vue';
+
+const title = ref('');
+const description = ref('');
+const ammount = ref<number | null>(null);
+const router = useRouter();
+function goBack() {
+  router.go(-1);
+}
+// eslint-disable-next-line no-undef
+function createCase() {
+  console.log(title.value);
+}
 </script>
